c1b4f94f93e666e4d753a713f4ccc55e7ddf5d7c
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-06-28 09:19:32 +0000

    Wed Jun 28 09:19:32 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,6 +1,9 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+LOWER_BOUND = 0
+UPPER_BOUND = 100
 def compute(x: int, y:int) -> int:
     """ Check if x and y are positive integers between 0 and 100 and if so return their sum. """
-    if not (0<=x <=100) or not(0<=y<=100):
+    if not (LOWER_BOUND <= x <= UPPER_BOUND) or not(LOWER_BOUND <= y<= UPPER_BOUND):
         raise ValueError("Both integers must be positive and between 0 and 100.")
     return x+y
