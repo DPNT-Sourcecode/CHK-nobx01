adce63be6268208bd1eb343991f65f2bbfbcff00
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-06-28 08:46:56 +0000

    Wed Jun 28 08:46:56 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x: int, y:int) -> int:
+    """ Check if x and y are positive integers between 0 and 100 and if so return their sum. """
+    if not (0<=x <=100) or not(0<=y<=100):
+        raise ValueError("Both integers must be positive and between 0 and 100.")
+    return x+y
+
