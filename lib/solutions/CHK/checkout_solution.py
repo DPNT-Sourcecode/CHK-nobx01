86c397190e696e400dadfd94ce0512609a2a1f18
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-06-28 12:46:47 +0000

    Wed Jun 28 12:46:47 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,27 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+PRICES = {
+    'A':{'price': 50, 'offer': {'quantity': 3, 'price': 130}},
+    'B':{'price': 30, 'offer': {'quantity': 2, 'price': 45}},
+    'C':{'price': 20, 'offer': None},
+    'D':{'price': 15, 'offer': None},
+}
+
+
+
+def clean_input(input: str) -> str | None:
+    if not isinstance(input, str) or len(input) == 0:
+        return
+    input = input.replace(' ', '').upper()
+    if not input.isalpha():
+        return
+    available_items = set(PRICES.keys())
+    if not set(input).issubset(available_items):
+        return
+    return input
+
 def checkout(skus):
     raise NotImplementedError()
+
