5665f19f80715d5a043700d58fbc7bb9dfc5677c
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-06-28 09:49:53 +0000

    Wed Jun 28 09:49:53 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name: str) -> str:
-    return "Hello, World!"
+    if not isinstance(friend_name, str) or len(input) == 0:
+        return "Hello, World!"
+    return 
+
