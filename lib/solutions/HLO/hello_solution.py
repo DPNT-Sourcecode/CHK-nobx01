83930ab21888da874b426f13ab347a251007bb8a
sbx_user1051 <sbx_user1051@169.254.21.221>
2023-06-28 09:52:53 +0000

    Wed Jun 28 09:52:53 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,8 +2,14 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
+
+BASE_STRING = "Hello, "
+DEFAULT = "World"
+DELIMETER = "!"
+
 def hello(friend_name: str) -> str:
     if not isinstance(friend_name, str) or len(input) == 0:
-        return "Hello, World!"
-    return 
+        return f"{BASE_STRING}{DEFAULT}{DELIMETER}"
+    return f"{BASE_STRING}{friend_name}{DELIMETER}"
+
 
