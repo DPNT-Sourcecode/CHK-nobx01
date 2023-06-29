4675983c5cd7ea0655c74b72e427aba230215884
sbx_user1051 <sbx_user1051@169.254.11.45>
2023-06-29 09:27:22 +0000

    Thu Jun 29 09:27:22 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -7,11 +7,15 @@ class TestCheckout():
         assert checkout_solution.clean_and_check_input('A--B CC D   B_A') == 'ABCCDBA'
         assert checkout_solution.clean_and_check_input('A--b cC D   B_A') == None
         assert checkout_solution.clean_and_check_input('A_4 D') == None
-        assert checkout_solution.clean_and_check_input('A--B CC D   B_A E') == None
+        assert checkout_solution.clean_and_check_input('A--B CC D   B_A F') == None
 
     def test_count_items(self):
         assert checkout_solution.count_items('ABBCCDDC') == {'A': 1, 'B': 2, 'C': 3, 'D':2}
 
+    def test_apply_free_items(self):
+        input = checkout_solution.clean_and_check_input('B'*2+'E'*7)
+        counted_items = checkout_solution.count_items(input)
+        assert checkout_solution.apply_free_items(counted_items) == {'B':0, 'E':7}
     # def test_checkout(self):
     #     assert checkout_solution.checkout('') == 0
     #     assert checkout_solution.checkout('ABE') == -1
