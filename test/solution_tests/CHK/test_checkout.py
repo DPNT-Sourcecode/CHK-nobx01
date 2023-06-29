e7217c675bd6514c6639dce86d60a31f397efc81
sbx_user1051 <sbx_user1051@169.254.11.45>
2023-06-29 14:13:28 +0000

    Thu Jun 29 14:13:28 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -50,25 +50,31 @@ class TestCheckout():
         assert checkout_solution.checkout('A'*5 + 'B'*5 + 'CD') == 355
         assert checkout_solution.checkout('A' + 'B' * 2) == 95
         assert checkout_solution.checkout('A'*11 + 'B'*7 + 'C'*4 + 'D'*4) == 755
-        #
-        # # no qualified offers
-        # assert checkout_solution.checkout('ABE') == 120
-        # # free offers
-        # assert checkout_solution.checkout('ABEE') == 130
-        # # free and special offers
-        # assert checkout_solution.checkout('B'*2 + 'E'*7) == 280
-        # assert checkout_solution.checkout('A'*2 + 'B'*6 + 'E'*7) == 455
-        # # multiple non-free special offers
-        # assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4) == 885
-        # assert checkout_solution.checkout('A'*14 + 'B'*5 + 'C'*2 + 'D'*2) == 770
-        # # round-3 checks
-        # assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*2) == 905
-        # assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*5) == 925
-        # assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*6) == 925
-        # assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*7) == 935
-        # assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*8) == 945
-        # assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*9) == 945
-        # assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*10) == 955
+
+        # no qualified offers
+        assert checkout_solution.checkout('ABE') == 120
+        # free offers
+        assert checkout_solution.checkout('ABEE') == 130
+        # free and special offers
+        assert checkout_solution.checkout('B'*2 + 'E'*7) == 280
+        assert checkout_solution.checkout('A'*2 + 'B'*6 + 'E'*7) == 455
+        # multiple non-free special offers
+        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4) == 885
+        assert checkout_solution.checkout('A'*14 + 'B'*5 + 'C'*2 + 'D'*2) == 770
+        # round-3 checks
+        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*2) == 905
+        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*5) == 925
+        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*6) == 925
+        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*7) == 935
+        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*8) == 945
+        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*9) == 945
+        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*10) == 955
+        # checks for round 4
+        assert checkout_solution.checkout('H'*38) == 3*80 + 1*45 + 3*10
+        assert checkout_solution.checkout('V' * 16) == 5*130 + 1*50
+        assert checkout_solution.checkout('V' * 17) == 5 * 130 + 1 * 90
+        assert checkout_solution.checkout('R' * 7 + 'Q'*9) == 7 * 50 + 2*80 + 30
+
 
 
 
