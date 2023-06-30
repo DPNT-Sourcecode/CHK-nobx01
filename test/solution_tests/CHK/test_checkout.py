ea85c1162010f16f518b7d8c0656f180030947a9
sbx_user1051 <sbx_user1051@169.254.18.5>
2023-06-30 11:45:43 +0000

    Fri Jun 30 11:45:43 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -69,6 +69,23 @@ class TestCheckout():
             'A' * 5 + 'B' * 5 + 'CD',
             'A' + 'B' * 2,
             'A' * 11 + 'B' * 7 + 'C' * 4 + 'D' * 4,
+            'ABE',
+            'ABEE',
+            'B' * 2 + 'E' * 7,
+            'A' * 2 + 'B' * 6 + 'E' * 7,
+            'A' * 14 + 'B' * 7 + 'C' * 4 + 'D' * 4,
+            'A' * 14 + 'B' * 5 + 'C' * 2 + 'D' * 2,
+            'A' * 14 + 'B' * 7 + 'C' * 4 + 'D' * 4 + 'F' * 2,
+            'A' * 14 + 'B' * 7 + 'C' * 4 + 'D' * 4 + 'F' * 5,
+            'A' * 14 + 'B' * 7 + 'C' * 4 + 'D' * 4 + 'F' * 6,
+            'A' * 14 + 'B' * 7 + 'C' * 4 + 'D' * 4 + 'F' * 7,
+            'A' * 14 + 'B' * 7 + 'C' * 4 + 'D' * 4 + 'F' * 8,
+            'A' * 14 + 'B' * 7 + 'C' * 4 + 'D' * 4 + 'F' * 9,
+            'A' * 14 + 'B' * 7 + 'C' * 4 + 'D' * 4 + 'F' * 10,
+            'H' * 38,
+            'V' * 16,
+            'V' * 17,
+            'R' * 7 + 'Q' * 9,
         ]
         expected_outputs = [
             0,
@@ -76,35 +93,29 @@ class TestCheckout():
             355,
             95,
             755,
+            120,
+            130,
+            280,
+            455,
+            885,
+            770,
+            905,
+            925,
+            925,
+            935,
+            945,
+            945,
+            955,
+            3 * 80 + 1 * 45 + 3 * 10,
+            5 * 130 + 1 * 50,
+            5 * 130 + 1 * 90,
+            7 * 50 + 2 * 80 + 30,
         ]
         for input, expected in zip(inputs, expected_outputs):
             assert checkout(input) == expected
 
 
 
-
-
-
-
-        # assert checkout() ==
-        # assert checkout() ==
-        # assert checkout('ABE') == 120
-        # assert checkout('ABEE') == 130
-        # assert checkout('B'*2 + 'E'*7) == 280
-        # assert checkout('A'*2 + 'B'*6 + 'E'*7) == 455
-        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4) == 885
-        # assert checkout('A'*14 + 'B'*5 + 'C'*2 + 'D'*2) == 770
-        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*2) == 905
-        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*5) == 925
-        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*6) == 925
-        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*7) == 935
-        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*8) == 945
-        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*9) == 945
-        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*10) == 955
-        # assert checkout('H'*38) == 3*80 + 1*45 + 3*10
-        # assert checkout('V' * 16) == 5*130 + 1*50
-        # assert checkout('V' * 17) == 5 * 130 + 1 * 90
-        # assert checkout('R' * 7 + 'Q'*9) == 7 * 50 + 2*80 + 30
 
 
 
