c164b8dc70e1d19592a742db575dda0cc1fff58b
sbx_user1051 <sbx_user1051@169.254.18.5>
2023-06-30 11:42:43 +0000

    Fri Jun 30 11:42:43 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,6 +1,7 @@
 from solutions.CHK import checkout_solution
 from solutions.CHK.checkout_solution import apply_free_items, PRICES
-from solutions.CHK.checkout_solution import clean_and_check_input, count_items, calculate_items_eligible_for_group_discount
+from solutions.CHK.checkout_solution import clean_and_check_input, count_items, \
+    calculate_items_eligible_for_group_discount, checkout
 
 
 class TestCheckout():
@@ -57,44 +58,54 @@ class TestCheckout():
             assert counted_items==expected_counts
 
 
+    def test_checkout_single_items(self):
+        for item, expected in zip(PRICES.keys(), [PRICES[item]['price'] for item in PRICES.keys()]):
+            assert checkout(item) == expected
 
     def test_checkout(self):
-        # empty string
-        assert checkout_solution.checkout('') == 0
-        # invalid input
-        assert checkout_solution.checkout('ABCa') == -1
+        inputs = [
+            '',
+            'ABCa',
+            'A' * 5 + 'B' * 5 + 'CD',
+            'A' + 'B' * 2,
+            'A' * 11 + 'B' * 7 + 'C' * 4 + 'D' * 4,
+        ]
+        expected_outputs = [
+            0,
+            -1,
+            355,
+            95,
+            755,
+        ]
+        for input, expected in zip(inputs, expected_outputs):
+            assert checkout(input) == expected
 
-        # single items
-        for item, expected in zip(PRICES.keys(), [PRICES[item]['price'] for item in PRICES.keys()]):
-            assert checkout_solution.checkout(item) == expected
 
-        assert checkout_solution.checkout('A'*5 + 'B'*5 + 'CD') == 355
-        assert checkout_solution.checkout('A' + 'B' * 2) == 95
-        assert checkout_solution.checkout('A'*11 + 'B'*7 + 'C'*4 + 'D'*4) == 755
 
-        # no qualified offers
-        assert checkout_solution.checkout('ABE') == 120
-        # free offers
-        assert checkout_solution.checkout('ABEE') == 130
-        # free and special offers
-        assert checkout_solution.checkout('B'*2 + 'E'*7) == 280
-        assert checkout_solution.checkout('A'*2 + 'B'*6 + 'E'*7) == 455
-        # multiple non-free special offers
-        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4) == 885
-        assert checkout_solution.checkout('A'*14 + 'B'*5 + 'C'*2 + 'D'*2) == 770
-        # round-3 checks
-        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*2) == 905
-        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*5) == 925
-        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*6) == 925
-        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*7) == 935
-        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*8) == 945
-        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*9) == 945
-        assert checkout_solution.checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*10) == 955
-        # checks for round 4
-        assert checkout_solution.checkout('H'*38) == 3*80 + 1*45 + 3*10
-        assert checkout_solution.checkout('V' * 16) == 5*130 + 1*50
-        assert checkout_solution.checkout('V' * 17) == 5 * 130 + 1 * 90
-        assert checkout_solution.checkout('R' * 7 + 'Q'*9) == 7 * 50 + 2*80 + 30
+
+
+
+
+        # assert checkout() ==
+        # assert checkout() ==
+        # assert checkout('ABE') == 120
+        # assert checkout('ABEE') == 130
+        # assert checkout('B'*2 + 'E'*7) == 280
+        # assert checkout('A'*2 + 'B'*6 + 'E'*7) == 455
+        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4) == 885
+        # assert checkout('A'*14 + 'B'*5 + 'C'*2 + 'D'*2) == 770
+        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*2) == 905
+        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*5) == 925
+        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*6) == 925
+        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*7) == 935
+        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*8) == 945
+        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*9) == 945
+        # assert checkout('A'*14 + 'B'*7 + 'C'*4 + 'D'*4 + 'F'*10) == 955
+        # assert checkout('H'*38) == 3*80 + 1*45 + 3*10
+        # assert checkout('V' * 16) == 5*130 + 1*50
+        # assert checkout('V' * 17) == 5 * 130 + 1 * 90
+        # assert checkout('R' * 7 + 'Q'*9) == 7 * 50 + 2*80 + 30
+
 
 
 
